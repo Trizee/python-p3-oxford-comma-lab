180a5a9b7580eb38ca6933f2aa7caeda544f3916
Tri <trihoang09@gmail.com>
2023-09-18 13:09:52 -0600

    completed lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,13 @@
 def oxford_comma(items):
-    return None
+    if len(items) > 2:
+        items[-1] = 'and ' + items[-1]
+        x = ', '.join(items)
+        return x
+    elif len(items) == 2:
+        items[-1] = 'and ' + items[-1]
+        x = ' '.join(items)
+        return x
+    else:
+        return ' '.join(items)
+
+oxford_comma(["a", "b", "c"])
